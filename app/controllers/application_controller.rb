5cbc708c3917971fe9e30a623650e2d81cdcc96a
jimhotchkiss <jimbohtochkiss@yahoo.com>
2018-04-29 19:39:46 -0700

    Added to application controller, issue, index.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,10 +9,12 @@ class ApplicationController < ActionController::Base
   end
 
   def logged_in?
-    session[:user_id]
+      !current_user.nil?
   end
 
   def current_user
-    @current_user ||= User.find_by(session[:user_id]) if session[:user_id]
-  end
+   return unless session[:user_id]
+   @current_user ||= User.find(session[:user_id])
+ end
+
 end
--- app/models/issue.rb
+++ app/models/issue.rb
@@ -2,12 +2,13 @@ class Issue < ApplicationRecord
   has_many :category_issues
   has_many :categories, through: :category_issues
   has_many :comments, dependent: :destroy
-  belongs_to :user
+
 
   validates :title, presence: true
   validates :description, presence: true, length: {maximum: 150}
   validates :solution, presence: true
   validates :category_ids, presence: true
+
 
 
 
